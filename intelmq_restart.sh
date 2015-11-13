238901f3c82170c7b13d6c429c7ff9f0a7a7d952
Mauro Silva <mauro.a.silva@gmail.com>
2015-11-13 16:42:20 +0000

    First push

---
+++ intelmq_restart.sh
@@ -0,0 +1,11 @@
+pip uninstall intelmq
+rm -rf /opt/intelmq
+python3.4 setup.py install
+cp /home/dev/intelmq_configs/* /opt/intelmq/etc/
+
+chmod -R 0770 /opt/intelmq
+chown -R intelmq.intelmq /opt/intelmq
+
+echo 'export PATH="$PATH:$HOME/bin"' > /opt/intelmq/.profile
+echo 'export INTELMQ_PYTHON=/usr/bin/python3.4' >> /opt/intelmq/.profile
+
